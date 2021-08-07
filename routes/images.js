2d2416d3d22516404caab83f87408045f60cc75c
mchowdam <mchowdam@woundtech.net>
2021-08-07 14:53:28 +0530

    added get end point

--- routes/images.js
+++ routes/images.js
@@ -1,23 +1,23 @@
-const router = require('express').Router();
-const updload = require('../utils/multer');
-const {uploadImages} = require('../services/imageService');
+const router = require("express").Router();
+const updload = require("../utils/multer");
+const { uploadImages, getImages } = require("../services/imageService");
 
-router.post('/', updload.array('image', 20), async (req, res, next) => {
+router.post("/", updload.array("image", 20), async (req, res, next) => {
   try {
     const data = await uploadImages(req?.files, req.body);
-    return res.status(200).json(data);
+    return res.status(201).json(data);
   } catch (e) {
-    console.log('exception', e.message);
+    console.log("exception", e.message);
     next(e);
   }
 });
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    return res.send('');
+    return res.status(200).json(getImages);
   } catch (err) {
     console.log(err);
-    next({ status: 400, message: 'Failed to fetch users' });
+    next({ status: 400, message: "Failed to fetch users" });
   }
 });
 
--- services/imageService.js
+++ services/imageService.js
@@ -1,15 +1,17 @@
 const Image = require("../models/image");
 const uploadImages = async (files, payload) => {
-    const data = files.map((file, i) => {
-        return {
-            description: payload.description[i],
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            imageUrl: file?.location,
-            image: file?.originalname,
-            contentType: file?.contentType
-        }
-    })
-    return await Image.insertMany(data);
-}
-module.exports = {uploadImages};
+  const data = files.map((file, i) => {
+    return {
+      description: payload.description[i],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      imageUrl: file?.location,
+      image: file?.originalname,
+      contentType: file?.contentType,
+    };
+  });
+  return await Image.insertMany(data);
+};
+
+const getImages = async () => Image.find().sort({ updatedAt: -1 });
+module.exports = { uploadImages, getImages };
